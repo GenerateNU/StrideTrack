93a714bc93676d132b7e98aa28d3e697ed7bf59a
samuelbaldwin05 <sambaldwin2005@gmail.com>
2026-01-07 10:07:20 -0500

    Update theme configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,6 +13,7 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       colors: {
+        /* These map CSS variables from index.css to Tailwind utility classes for the colors*/
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         card: {
