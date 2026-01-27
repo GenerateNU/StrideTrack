272f627b2c435b39385fee1759903821a71f9d76
dboghra <boghra.d@northeastern.edu>
2026-01-27 14:23:22 -0500

    Add Supabase client and Google OAuth login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import { ExamplePage } from './pages/ExamplePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ExamplePage } from "./pages/ExamplePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ExamplePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
