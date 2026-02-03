4b31a379a2d98177e48f0497e7cdd3473f962889
Michael Maaseide <maaseidemichael@gmail.com>
2026-02-03 17:37:51 -0500

    login page commented

--- frontend/src/pages/login_page.tsx
+++ frontend/src/pages/login_page.tsx
@@ -1,22 +1,22 @@
-import { useEffect } from "react";
-import { supabase } from "../lib/supabaseClient";
+// import { useEffect } from "react";
+// import { supabase } from "../lib/supabase";
 
-export default function LoginPage() {
-  useEffect(() => {
-    const checkSession = async () => {
-      const { data } = await supabase.auth.getSession();
-      console.log("SESSION:", data.session);
-    };
+// export default function LoginPage() {
+//   useEffect(() => {
+//     const checkSession = async () => {
+//       const { data } = await supabase.auth.getSession();
+//       console.log("SESSION:", data.session);
+//     };
 
-    checkSession();
-  }, []);
+//     checkSession();
+//   }, []);
 
-  const handleGoogle = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: "google",
-      options: { redirectTo: window.location.origin },
-    });
-  };
+//   const handleGoogle = async () => {
+//     await supabase.auth.signInWithOAuth({
+//       provider: "google",
+//       options: { redirectTo: window.location.origin },
+//     });
+//   };
 
-  return <button onClick={handleGoogle}>Sign in with Google</button>;
-}
+//   return <button onClick={handleGoogle}>Sign in with Google</button>;
+// }
