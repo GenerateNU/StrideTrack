88dc69d5f6bdc4d542675e088a03b436bc8ddb12
samuelbaldwin05 <sambaldwin2005@gmail.com>
2026-02-11 21:13:22 -0500

    Combined migrations

--- supabase/migrations/20260208211100_create_athletes.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE athletes (
-    athlete_id UUID PRIMARY KEY,
-    coach_id UUID REFERENCES coaches(coach_id),
-    name VARCHAR(50),
-    height NUMERIC,
-    weight NUMERIC,
-    created_at TIMESTAMPTZ
-);
