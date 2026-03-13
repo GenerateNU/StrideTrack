9fc2a510e8dcd025df590c84ced9e503bf0c4580
samuelbaldwin05 <sambaldwin2005@gmail.com>
2026-03-13 14:52:29 -0400

    Moved otel.ts to lib

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import "./otel";
+import "./lib/otel";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
