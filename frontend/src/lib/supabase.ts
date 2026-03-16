afbef8258ec9343de34d75407067eba0640f84f1
samuelbaldwin05 <sambaldwin2005@gmail.com>
2026-03-16 18:28:13 -0400

    Updated supabase variable names

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 );
 // createClient used ot be createBrowserClient and supabase-js used to be ssr
