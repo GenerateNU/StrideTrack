c40e319901c3a1d26cd395f107d2eef5e9e28f1e
shreya-m9 <mookherjee.s@northeastern.edu>
2026-03-19 21:10:42 -0400

    fixed linting

--- backend/app/api.py
+++ backend/app/api.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, Depends
 from supabase._async.client import AsyncClient
 
 from app.core.supabase import get_async_supabase
-from app.routes.bosco_routes import router as bosco_router
 from app.routes.athlete_routes import router as athlete_router
 from app.routes.auth_routes import router as auth_router
+from app.routes.bosco_routes import router as bosco_router
 from app.routes.csv_routes import router as csv_router
 from app.routes.example_routes import router as example_router
 from app.routes.hurdle_routes import router as hurdle_router
