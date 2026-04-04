53ae52ab698f01ad7f3b1136782fde1b0ff1459b
shreya-m9 <mookherjee.s@northeastern.edu>
2026-04-04 14:20:59 -0400

    fixed schema

--- frontend/src/types/event.types.ts
+++ frontend/src/types/event.types.ts
@@ -1,18 +1,22 @@
-export type EventTypeEnum =
-  | "sprint_60m"
-  | "sprint_100m"
-  | "sprint_200m"
-  | "sprint_400m"
-  | "hurdles_60m"
-  | "hurdles_110m"
-  | "hurdles_100m"
-  | "hurdles_400m"
-  | "hurdles_partial"
-  | "long_jump"
-  | "triple_jump"
-  | "high_jump"
-  | "bosco_test"
-  | "reaction_time_test";
+import { z } from "zod";
+export const eventTypeEnum = z.enum([
+  "sprint_60m",
+  "sprint_100m",
+  "sprint_200m",
+  "sprint_400m",
+  "hurdles_60m",
+  "hurdles_110m",
+  "hurdles_100m",
+  "hurdles_400m",
+  "hurdles_partial",
+  "long_jump",
+  "triple_jump",
+  "high_jump",
+  "bosco_test",
+  "reaction_time_test",
+]);
+
+export type EventTypeEnum = z.infer<typeof eventTypeEnum>;
 
 export type HurdleTargetEvent =
   | "hurdles_60m"
