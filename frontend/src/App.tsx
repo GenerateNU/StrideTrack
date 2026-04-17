e67212e15fff8b2c37a98d13402d821978649490
samuelbaldwin05 <sambaldwin2005@gmail.com>
2026-04-17 14:35:27 -0400

    lazy load additional pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,23 @@
 import { ProtectedRoute } from "@/components/auth/ProtectedRoute";
 import { AppLayout } from "@/components/layout/AppLayout";
-import AthleteProfilePage from "@/pages/AthleteProfilePage";
-import HistoryPage from "@/pages/HistoryPage";
-import HomePage from "@/pages/HomePage";
-import LoginPage from "@/pages/LoginPage";
-import NotFoundPage from "@/pages/NotFoundPage";
-import RecordRunPage from "@/pages/RecordRunPage";
-import RunAnalysisPage from "@/pages/RunAnalysisPage";
-import VisualizationsPage from "@/pages/VisualizationsPage";
+import { QueryLoading } from "@/components/ui/QueryLoading";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const AthleteProfilePage = lazy(() => import("@/pages/AthleteProfilePage"));
+const HistoryPage = lazy(() => import("@/pages/HistoryPage"));
+const HomePage = lazy(() => import("@/pages/HomePage"));
+const LoginPage = lazy(() => import("@/pages/LoginPage"));
+const NotFoundPage = lazy(() => import("@/pages/NotFoundPage"));
+const RecordRunPage = lazy(() => import("@/pages/RecordRunPage"));
+const RunAnalysisPage = lazy(() => import("@/pages/RunAnalysisPage"));
+const VisualizationsPage = lazy(() => import("@/pages/VisualizationsPage"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+      <Suspense fallback={<QueryLoading />}>
+        <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route
           path="*"
@@ -43,6 +47,7 @@ function App() {
           }
         />
       </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
